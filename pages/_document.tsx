246ae61f7ef65693e6351330fa626d0c249a2f2c
James Wasmer <jameswasmer@gmail.com>
2023-02-25 10:14:30 -0500

    Remove mapbox imports

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head> 
+        <link href='https://api.mapbox.com/mapbox-gl-js/v2.13.0/mapbox-gl.css' rel='stylesheet' />
+      </Head>
       <body>
         <Main />
         <NextScript />
