bae6e5c8a0187d1d6f65953d5d5d76ce313fed7d
James Wasmer <jameswasmer@gmail.com>
2023-02-25 10:16:15 -0500

    Build basic mapbox map component

---
+++ components/BackdropMap.tsx
@@ -0,0 +1,34 @@
+import mapboxgl from 'mapbox-gl';
+import React, { useState, useEffect, useRef } from "react";
+
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN ?? ''
+
+export default function BackdropMap() {
+  const mapContainer = useRef(null)
+    const map = useRef<mapboxgl.Map | null>(null)
+    const [lng, setLng] = useState(-70.9)
+    const [lat, setLat] = useState(42.35)
+    const [zoom, setZoom] = useState(9)
+
+  useEffect(() => {
+    if (map.current) return;
+
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current === undefined || mapContainer.current === null ? 
+      "" : mapContainer.current,
+      style: "mapbox://styles/mapbox/streets-v12",
+      center: [lng, lat],
+      zoom: zoom
+    })
+  })
+
+  useEffect(() => {
+    if (!map.current) return;
+
+    map.current.on("move", () => {
+      setLng(Number(map.current?.getCenter().lng.toFixed(4)))
+      setLat(Number(map.current?.getCenter().lat.toFixed(4)))
+      setZoom(Number(map.current?.getZoom().toFixed(2)))
+    })
+  })
+}
