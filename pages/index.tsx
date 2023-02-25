bf98021fd87c023810df8f1c0e0c8fe33042514c
James Wasmer <jameswasmer@gmail.com>
2023-02-25 10:14:54 -0500

    Import mapbox css stylesheet

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react'
 import Account from '../components/Account'
 import AuthModal from '../components/AuthModal'
 import style from "../styles/Home.module.css"
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 const Home = () => {
   const session = useSession()
