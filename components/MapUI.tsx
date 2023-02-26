28a9e05747eae3627dd0b87ad3f0ba781b5b7b00
James Wasmer <jameswasmer@gmail.com>
2023-02-26 14:42:34 -0500

    Build controlled search form for address search

--- components/MapUI.tsx
+++ components/MapUI.tsx
@@ -1,5 +1,20 @@
+import { useState, ChangeEvent, ChangeEventHandler } from "react"
+import styles from "../styles/MapUI.module.css"
+
 export default function MapUI() {
+  const [searchText, setSearchText] = useState<string>('')
+
+  const handleSearchTextChange: ChangeEventHandler<HTMLInputElement> = (
+    event: ChangeEvent<HTMLInputElement>
+  ): void => {
+    setSearchText(event.target.value)
+  }
+
   return (
-    <div> UI </div>
+    <header>
+      <form>
+        <input className={styles.search} type="text" value={searchText} onChange={() => {handleSearchTextChange}}></input>
+      </form>
+    </header>
   )
 }
