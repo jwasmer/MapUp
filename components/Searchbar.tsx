b267668df26ca91de90cebe1565131da5b5e981a
James Wasmer <jameswasmer@gmail.com>
2023-02-26 19:28:57 -0500

    Build controlled form for searchbar

---
+++ components/Searchbar.tsx
@@ -0,0 +1,16 @@
+import { useState, ChangeEvent, ChangeEventHandler } from "react"
+import styles from "../styles/Searchbar.module.css"
+
+export default function Searchbar() {
+  const [searchText, setSearchText] = useState<string>('')
+
+  const handleSearchTextChange: ChangeEventHandler<HTMLInputElement> = (
+    event: ChangeEvent<HTMLInputElement>
+  ): void => {
+    setSearchText(event.target.value)
+  }
+
+  return (
+    <input className={styles.search} type="text" value={searchText} onChange={handleSearchTextChange}></input>
+  )
+}
