a55f7f841cd91871221df2ac960331efe669f32e
James Wasmer <jameswasmer@gmail.com>
2023-02-26 20:45:58 -0500

    Render props.icon as dropdown button

--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -1,11 +1,17 @@
+import { useState } from "react"
 import SettingsIcon from "./icons/SettingsIcon"
 import styles from "../styles/Dropdown.module.css"
+import { DropdownProps } from "@/utils/interface"
 
-export default function Dropdown() {
+export default function Dropdown(props: DropdownProps) {
+  const [showDropdown, setShowDropdown] = useState<boolean>(false)
+
+  console.log("Props in Dropdown:", props)
+
   return (
     <li className={styles.dropdown}>
-      <button className={styles.button}>
-        <SettingsIcon color={"#FFFFFF"} />
+      <button className={styles.button} onClick={() => setShowDropdown((prevState) => {return !prevState})}>
+        { props.icon }
       </button>
     </li>
   )
