f4957295d91d6219eacc2a80eed4dd64939a0774
James Wasmer <jameswasmer@gmail.com>
2023-02-27 19:34:38 -0500

    Remove console.log from SettingsIcon

--- components/icons/SettingsIcon.tsx
+++ components/icons/SettingsIcon.tsx
@@ -1,7 +1,6 @@
 import { ColorProps } from "@/utils/interface"
 
 export default function SettingsIcon({ color }: ColorProps) {
-  console.log(color)
 
   return (
     <span className={"material-symbols-outlined"} style={{color}}>
