43d27f0c39bffa50e4b1f4c44384509f4f61b47a
James Wasmer <jameswasmer@gmail.com>
2023-02-27 19:34:20 -0500

    Pass through MenuItem props in MapUI

--- components/MapUI.tsx
+++ components/MapUI.tsx
@@ -2,6 +2,9 @@ import styles from "../styles/MapUI.module.css"
 import DownChevronIcon from "./icons/DownChevronIcon"
 import Searchbar from "./Searchbar"
 import Dropdown from "./Dropdown"
+import Menu from "./Menu"
+import MenuItem from "./MenuItem"
+import SettingsIcon from "./icons/SettingsIcon"
 
 export default function MapUI() {
 
@@ -9,9 +12,13 @@ export default function MapUI() {
     <header className={styles.nav}>
       <Searchbar />
       <menu className={styles.menuWrapper}>
-        <Dropdown 
-          icon={<DownChevronIcon color={"#FFFFFF"} />}
-        />
+        <Dropdown icon={<DownChevronIcon color={"#FFFFFF"} />}>
+          <Menu>
+            <MenuItem icon={<SettingsIcon color="#000000" />} />
+            <MenuItem icon={<SettingsIcon color="#000000" />} />
+            <MenuItem icon={<SettingsIcon color="#000000" />} />
+          </Menu>
+        </Dropdown>
       </menu>
     </header>
   )
