3b3c61da726430b4270ac93457c8530d9060dedc
James Wasmer <jameswasmer@gmail.com>
2023-02-27 19:33:43 -0500

    Move MenuItem props up to MapUI

--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -1,16 +1,25 @@
 import { useState } from "react"
-import styles from "../styles/Dropdown.module.css"
 import { DropdownProps } from "@/utils/interface"
+import Menu from "./Menu"
+import MenuItem from "./MenuItem"
+import SettingsIcon from "./icons/SettingsIcon"
+import styles from "../styles/Dropdown.module.css"
 
 export default function Dropdown(props: DropdownProps) {
   const [open, setOpen] = useState<boolean>(false)
 
   return (
     <li className={styles.dropdown}>
-      <button className={styles.button}>
+      <button className={styles.button} onClick={() => {
+        setOpen(true)
+      }}>
         {props.icon}
-        {open && <Menu />}
       </button>
+      {open && 
+        <Menu>
+          {props.children}
+        </Menu>
+      }
     </li>
   )
 }
