5366bd1a8f09ba9e86d65f357346c07ebca0902c
James Wasmer <jameswasmer@gmail.com>
2023-03-03 22:22:26 -0500

    Add DisplayUserOrgs component to Organizations

--- pages/organizations/index.tsx
+++ pages/organizations/index.tsx
@@ -1,59 +1,13 @@
-import { useState, useEffect } from "react"
-import { useSupabaseClient, useSessionContext } from '@supabase/auth-helpers-react'
-import { Database } from "@/utils/supabase"
-import { OrgData } from "@/utils/interface"
+import { useState } from "react"
 import DisplayUserOrgs from "@/components/DisplayUserOrganizations"
 import CreateOrganization from "@/components/CreateOrganization"
-type Organizations = Database['organizations']
 
 export default function Organizations() {
-  const supabase = useSupabaseClient<Database>()
-  const { session } = useSessionContext()
-  const [loading, setLoading] = useState(true)
-  const [orgData, setOrgData] = useState<OrgsData[] | null>(null)
   const [search, setSearch] = useState<string>('')
-
-  useEffect(() => {
-    console.log(session)
-    if (session) getUserOrgs()
-  }, [session])
-
-  async function getUserOrgs() {
-    try {
-      setLoading(true)
-      if (!session) throw new Error('No user')
-      
-      let { data, error, status } = await supabase
-        .from('join_users_organizations')
-        .select()
-        .eq('user_id', session.user.id)
-        .select(`organization_id, user_organization_role, 
-        organizations (
-          organization_name
-        )`)
-
-      console.log(data)
-
-      if (data) {
-        setOrgData(data as OrgsData[])
-      }
-
-      if (error && status !== 406) {
-        console.log("Error fetching from join_users_organizations table")
-        throw Error
-      }
-
-    } catch (error) {
-      alert ('Error loading organizations data!')
-      console.log(error)
-    } finally {
-      setLoading(false)
-    }
-  }
 
   return (
     <>
-      {session && <div className="">      
+      <div className="">      
         <h2>
           Your Organizations
         </h2>
@@ -63,7 +17,7 @@ export default function Organizations() {
           <input id="search-orgs" type="text" value={search} placeholder="Search for organizations..." onChange={(event) => setSearch(event.target.value)} />
         </form>
         <CreateOrganization />
-      </div>}
+      </div>
     </>
   )
 }
