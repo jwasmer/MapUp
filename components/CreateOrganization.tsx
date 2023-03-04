86f4c6573e441546d8ef8ccdf765349d5d168338
James Wasmer <jameswasmer@gmail.com>
2023-03-03 19:40:09 -0500

    Build component to post new org to db

--- components/CreateOrganization.tsx
+++ components/CreateOrganization.tsx
@@ -1,17 +1,20 @@
-import { useEffect, useState } from "react"
+import { useState, useEffect } from "react"
 import { Database } from "../utils/supabase"
 import { useSessionContext, useSupabaseClient } from "@supabase/auth-helpers-react"
+import { v4 as uuidv4 } from 'uuid'
 type Organizations = Database['organizations']
 
 export default function CreateOrganization() {
   const supabase = useSupabaseClient()
   const { session } = useSessionContext()
-  const [organization_id, setOrgId] = useState<Organizations['organization_id']>(null)
+  const organization_id = uuidv4()
+  const user_organization_id = uuidv4()
   const [organization_name, setOrgName] = useState<Organizations['organization_name']>('')
   const [loading, setLoading] = useState<boolean>(false)
   const [invitations, setInvitations] = useState('')
 
-  const createNewOrg = async ({organization_name}: {organization_name: Organizations['organization_name']}) => {
+  const createNewOrg = async (organization_name: Organizations['organization_name']) => {
+    event?.preventDefault()
     if (!organization_name) {
       alert('Organization name is a required field')
       return
@@ -21,15 +24,26 @@ export default function CreateOrganization() {
       setLoading(true)
       if (!session!.user) throw Error('No user')
 
-      const orgUpdates = {organization_name}
-      const joinUsersOrgsUpdates = {
+      const orgUpdates = {
+        organization_id,
+        organization_name
+      }
+      const joinUpdates = {
+        user_organization_id,
         organization_id,
         user_id: session?.user.id,
         user_organization_role: "admin"
       }
 
-      let { error } = await supabase.from('organizations').insert(orgUpdates)
-      if (error) throw error
+      const { data: orgUpdatesData, error: orgUpdatesError } = await supabase
+        .from('organizations')
+        .insert(orgUpdates)
+
+      const { data: joinUpdatesData, error: joinUpdatesError } = await supabase
+        .from('join_users_organizations')
+        .insert(joinUpdates)
+
+      if (orgUpdatesError || joinUpdatesError) throw Error
       alert('Organization created!')
     } catch (error) {
       alert('Error creating the organization!')
@@ -48,7 +62,7 @@ export default function CreateOrganization() {
         <form>
           <label htmlFor="org-name">Org Name: </label>
           <input id="org-name" type="text" value={organization_name} placeholder="Enter organization name..." onChange={(event) => setOrgName(event.target.value)} />
-          <button onClick={() => {}}>Submit new org</button>
+          <button onClick={() => {createNewOrg(organization_name)}}>{loading ? "Loading ..." : "Submit"}</button>
         </form>
         <form>
           <label htmlFor="invite-members">Invite members: </label>
