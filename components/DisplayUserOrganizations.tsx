b25e997f42938cfea11eece63d92d6f0b1c956e0
James Wasmer <jameswasmer@gmail.com>
2023-03-06 17:21:58 -0500

    Call delete_organization sql function in query

--- components/DisplayUserOrganizations.tsx
+++ components/DisplayUserOrganizations.tsx
@@ -1,7 +1,8 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, SyntheticEvent } from "react"
 import { useSupabaseClient, useSessionContext } from '@supabase/auth-helpers-react'
 import { Database } from "@/utils/supabase"
 import { OrgData } from "@/utils/interface"
+import Icon from "@/components/Icon"
 import styles from "@/styles/Organizations.module.css"
 
 export default function DisplayUserOrgs() {
@@ -20,12 +21,15 @@ export default function DisplayUserOrgs() {
         .channel('org_updates')
         .on('postgres_changes', { event: 'INSERT', schema: 'public', table: 'join_users_organizations', filter: `user_id=eq.${session.user.id}`}, () => {
           getUserOrgs()
+          console.log("INSERT registered by subscription")
         })
         .on('postgres_changes', { event: 'UPDATE', schema: 'public', table: 'join_users_organizations', filter: `user_id=eq.${session.user.id}`}, () => {
           getUserOrgs()
+          console.log("UDPATE registered by subscription")
         })
         .on('postgres_changes', { event: 'DELETE', schema: 'public', table: 'join_users_organizations', filter: `user_id=eq.${session.user.id}`}, () => {
           getUserOrgs()
+          console.log("DELETE registered by subscription")
         })
         .subscribe()
       
@@ -37,7 +41,17 @@ export default function DisplayUserOrgs() {
     if (orgData) {
       const buildOrgCards = (orgData: OrgData[]) => {
         return orgData.map((org) => {
-          return <div className={styles.org} key={org.organization_id}>{org.organizations.organization_name}</div>
+          return <div className={styles.org}>
+            <h3 key={org.organization_id}>
+              {org.organizations.organization_name}
+            </h3>
+            <button 
+              className={styles.deleteButton} 
+              id={org.organization_id!}
+              onClick={(event: SyntheticEvent) => {deleteOrg(event)}}>
+              <Icon name={"delete"} />
+            </button>
+          </div>
         })
       }
 
@@ -59,8 +73,6 @@ export default function DisplayUserOrgs() {
           organization_name
         )`)
 
-      console.log(data)
-
       if (data) {
         setOrgData(data as OrgData[])
       }
@@ -72,6 +84,28 @@ export default function DisplayUserOrgs() {
 
     } catch (error) {
       alert ('Error loading organizations data!')
+      console.log(error)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  async function deleteOrg(event: SyntheticEvent) {
+    const orgId = (event.target as HTMLElement).id || (event.target as HTMLElement).parentElement?.id
+    console.log("orgId:", orgId)
+
+    try {
+      setLoading(true)
+      if (!session) throw new Error('No user')
+
+      let { error, status } = await supabase
+        .rpc('delete_organization', { org_id: orgId, auth_id: session.user.id });
+
+      if (error && status !== 406) {
+        throw Error
+      }
+    } catch (error) {
+      alert ('Error deleting organization!')
       console.log(error)
     } finally {
       setLoading(false)
