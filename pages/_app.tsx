ccbac14b1b9d0fc53b1cfe98072b3aa031d6f1b3
James Wasmer <jameswasmer@gmail.com>
2023-03-14 19:09:21 -0600

    Move supabaseClient to file and import into _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,14 @@
 import '@/styles/globals.css'
-import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
 import { SessionContextProvider, Session } from '@supabase/auth-helpers-react'
 import { AppProps } from 'next/app'
 import { mapState, MapStateContext } from "../helpers/mapStateContext"
 import 'mapbox-gl/dist/mapbox-gl.css';
+import { supabaseClient } from '@/supabase/supabaseClient'
 
 export default function MyApp({
   Component,
   pageProps,
 }: AppProps<{ initialSession: Session }>) {
-
-  const supabaseUrl = (process.env.NEXT_PUBLIC_SUPABASE_URL as string)
-  const supabaseKey = (process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string)
-
-  const supabaseClient = createBrowserSupabaseClient({
-    supabaseUrl,
-    supabaseKey
-  })
 
   return (
     <SessionContextProvider supabaseClient={supabaseClient} initialSession={pageProps.initialSession}>
