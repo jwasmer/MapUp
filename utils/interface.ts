d8ddb509367e3e4adb75e7bfcb52347710175199
James Wasmer <jameswasmer@gmail.com>
2023-07-14 12:52:24 -0400

    Add type definitions for new nav menu components

--- utils/interface.ts
+++ utils/interface.ts
@@ -1,14 +1,17 @@
 import { PropsWithChildren } from "react"
 import { Session } from "@supabase/supabase-js"
 
-export interface IconProps {
-  color?: string
-  name: "arrow_drop_down" | "settings" | "navigate_next" | "add" | "delete" | "account_circle" | "manage_accounts" | "logout"
+export interface ToggleButtonProps {
+  iconTrue: React.ReactNode
+  iconFalse: React.ReactNode
+  colorTrue?: string
+  colorFalse?: string
 }
 
-export type MapButtonProps = PropsWithChildren<{
-  icon: React.ReactNode
-}>
+export interface IconProps {
+  color?: string
+  name: string
+}
 
 export interface MapContext {
   profile: string
@@ -19,12 +22,6 @@ export interface ProcessEnv {
   NEXT_PUBLIC_SUPABASE_URL: string
   NEXT_PUBLIC_SUPABASE_ANON_KEY: string
 }
-
-export type MenuItemProps = PropsWithChildren <{
-  iconLeft?: React.ReactNode
-  iconRight?: React.ReactNode
-  link?: string
-}>
 
 export interface OrgCard {
   orgData: OrgData
@@ -50,3 +47,28 @@ export interface SearchbarProps {
 export interface TestSessions {
   user?: Session
 }
+
+export type ActiveMenu = 'Map' | 'Events' | 'Organizations' | 'Account' | null
+
+export type MapButtonProps = PropsWithChildren<{
+  icon: React.ReactNode
+}>
+
+export type MenuItemProps = PropsWithChildren <{
+  iconLeft?: React.ReactNode
+  iconRight?: React.ReactNode
+  link?: string
+}>
+
+export type NavBarProps = PropsWithChildren <{
+  detailsFrameToggle: boolean
+  setDetailsFrameToggle: Function
+}>
+
+export type NavButtonProps = PropsWithChildren <{
+  setActiveMenu: Function
+  activeMenu: string | null
+  menuItemName: string
+  iconName: string
+  setDetailsFrameToggle: Function
+}>
