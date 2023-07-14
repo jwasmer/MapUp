a49b9e5fb4b6a3adffba7ba1424dc0ea847644ef
James Wasmer <jameswasmer@gmail.com>
2023-07-14 12:50:00 -0400

    Add polish to side bar nav menu

--- components/MapUI.tsx
+++ components/MapUI.tsx
@@ -1,19 +1,13 @@
 import { MouseEventHandler, useState } from "react";
 import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import styles from "../styles/MapUI.module.css";
-import Link from "next/link";
-import Icon from "./Icon";
-import Menu from "./Menu";
-import Searchbar from "./Searchbar";
-import MapButton from "./MapButton";
-import MenuItem from "./MenuItem";
+import NavBar from "./NavMenu/NavBar";
 import { Database } from "@/utils/supabase";
 type OrganizationRow = Database["public"]["Tables"]["organizations"]["Row"];
 type Result = Pick<OrganizationRow, "organization_id" | "organization_name">;
 
 export default function MapUI() {
   const supabase = useSupabaseClient<Database>();
-  const [detailsFrameToggle, setDetailsFrameToggle] = useState(false)
   const [results, setResults] = useState<Result[]>([]);
   const [loading, setLoading] = useState(false);
 
@@ -51,15 +45,11 @@ export default function MapUI() {
     }
   }
 
-  function toggleDetailsFrame() {
-    setDetailsFrameToggle(toggleState => !toggleState)
-  }
-
   return (
     <>
       <header className={styles.nav}>
         {/* <Searchbar runSearch={searchAddresses} data-cy="mapui-searchbar" /> */}
-        <MapButton icon={<Icon name={"arrow_drop_down"} />} data-cy="mapui-dropdown">
+        {/* <MapButton icon={<Icon name={"arrow_drop_down"} />} data-cy="mapui-dropdown">
           <Menu>
             <Link href="/account">
               <MenuItem iconLeft={<Icon color={"#FFFFFF"} name={"account_circle"} />} data-cy="mapui-account">
@@ -82,21 +72,9 @@ export default function MapUI() {
             </button>
           </Menu>
         </MapButton>
-        <MapButton icon={<Icon name={"manage_accounts"} />} data-cy="mapui-dropdown" />
+        <MapButton icon={<Icon name={"manage_accounts"} />} data-cy="mapui-dropdown" /> */}
       </header>
-      <div className={styles.sideBar}>
-        <div className={styles.sideBarNav}>
-          <button></button>
-          <Icon color={"var(--bg-accent)"} name={"arrow_left"} />
-          <Icon color={"var(--bg-accent)"} name={"arrow_left"} />
-          <Icon color={"var(--bg-accent)"} name={"arrow_left"} />
-        </div>
-        <div className={`${styles.detailsFrame} ${detailsFrameToggle ? styles.expanded : styles.collapsed}`}>
-        </div>
-      </div>
-      <button className={`${styles.sideBarViewBtn} ${detailsFrameToggle ? styles.expanded : styles.collapsed}`} onClick={toggleDetailsFrame}>
-        {detailsFrameToggle ? <Icon color={"var(--bg-accent)"} name={"arrow_left"} /> : <Icon color={"var(--bg-accent)"} name={"arrow_right"} />}
-      </button>
+      <NavBar />
     </>
   );
 }
